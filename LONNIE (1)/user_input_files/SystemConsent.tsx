0ed6f81749ec0803a3ae2925e95860196bd248f6
dhurd7313 <dhurd7313@gmail.com>
2026-04-27 09:12:39 +0700

    Update SystemConsent.tsx

--- LONNIE (1)/user_input_files/SystemConsent.tsx
+++ LONNIE (1)/user_input_files/SystemConsent.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "./useAuth";
-import { useAuth } from "../useAuth";
+import { useAuth } from "./useAuth";
 
 const SystemConsent = () => {
   const [accepted, setAccepted] = useState(false);
