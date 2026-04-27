d9cf68fba59a72acdf18242da46ca40f08a8c8f8
dhurd7313 <dhurd7313@gmail.com>
2026-04-27 09:08:48 +0700

    Update SystemConsent.tsx

--- LONNIE (1)/user_input_files/SystemConsent.tsx
+++ LONNIE (1)/user_input_files/SystemConsent.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useAuth } from "./useAuth";
 import { useAuth } from "../useAuth";
 
 const SystemConsent = () => {
