5649657f396360119426141728af4e91e8981c6c
dhurd7313 <dhurd7313@gmail.com>
2026-04-28 08:41:50 +0700

    Update Auth.tsx

--- LONNIE (1)/user_input_files/Auth.tsx
+++ LONNIE (1)/user_input_files/Auth.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./useAuth";
 
-const Auth = () => {
+export default function Auth() {
   const [isLogin, setIsLogin] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -124,6 +124,4 @@ const Auth = () => {
       </div>
     </div>
   );
-};
-
-export default Auth;  // <-- MAKE SURE THIS LINE EXISTS
+}
